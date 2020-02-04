db08c5fb92e1c7dc58a9c0e704d0b4b1a9836848
Samuel Cusi <scusi@gruporpp.com.pe>
2020-02-04 14:27:46 -0500

    Share results

--- app/src/main/java/com/example/android/navigation/GameOverFragment.kt
+++ app/src/main/java/com/example/android/navigation/GameOverFragment.kt
@@ -20,6 +20,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
@@ -35,6 +36,8 @@ class GameOverFragment : Fragment() {
             //it.findNavController().navigate(R.id.action_gameOverFragment2_to_gameFragment)
             it.findNavController().navigate(GameOverFragmentDirections.actionGameOverFragment2ToGameFragment())
         }
+        var args = GameWonFragmentArgs.fromBundle(arguments!!)
+        Toast.makeText(context, "NumCorrect: ${args.numCorrect}, NumQuestions: ${args.numQuestions}", Toast.LENGTH_LONG).show()
         return binding.root
     }
 }

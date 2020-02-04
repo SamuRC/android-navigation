c1585ae4f522930d3fb68fec4d573f1f9abbb13c
Samuel Cusi <scusi@gruporpp.com.pe>
2020-02-04 13:43:17 -0500

    Pass args

--- app/src/main/java/com/example/android/navigation/TitleFragment.kt
+++ app/src/main/java/com/example/android/navigation/TitleFragment.kt
@@ -21,9 +21,12 @@ class TitleFragment : Fragment() {
         val binding: FragmentTitleBinding = DataBindingUtil.inflate(
                 inflater, R.layout.fragment_title, container, false
         )
-        binding.playButton.setOnClickListener (
+        binding.playButton.setOnClickListener { view: View ->
+            view.findNavController().navigate(TitleFragmentDirections.actionTitleFragmentToGameFragment())
+        }
+        /* binding.playButton.setOnClickListener (
                 Navigation.createNavigateOnClickListener(R.id.action_titleFragment_to_gameFragment)
-        )
+        ) */
         /* binding.playButton.setOnClickListener { view: View ->
             // Navigation.findNavController(view).navigate(R.id.action_titleFragment_to_gameFragment)
             // replace with ktx
